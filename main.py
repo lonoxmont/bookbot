147630ee47b79d77ec12271d446955cb7ca27860
lonoxmont <lonoxmont@suptg.com>
2024-11-24 15:10:40 -0800

    made main.py spit out a relatively pretty report on one book, frankenstein by mary shelley

--- main.py
+++ main.py
@@ -2,21 +2,28 @@
 def count_letters(words):
     letter_count_dict = {}
     for char in words.lower():
-        if char in letter_count_dict:
-            letter_count_dict[char] += 1
-        else:
-            letter_count_dict[char] = 1
-    print(letter_count_dict)
+        if char.isalpha():
+            if char in letter_count_dict:
+                letter_count_dict[char] += 1
+            else:
+                letter_count_dict[char] = 1
+    return(letter_count_dict)
+
+
 def count_words(words):
     word_list = words.split()
     count = len(word_list)
-    print(count)
+    return(count)
     
 
 def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
-        count_words(file_contents)
-        count_letters(file_contents)
-    #print(file_contents)
+        word_count = count_words(file_contents)
+        letter_count = count_letters(file_contents)
+    print(f"--- Word count: {word_count} ---")
+    print("--- Character counts ---")
+    sorted_chars = sorted(letter_count.items(), key=lambda x: x[1], reverse=True)
+    for char, count in sorted_chars:
+        print(f"The '{char}' character appears {count} times")
 main()
